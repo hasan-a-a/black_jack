82401a0c5aa894183a1b12f6db7a25579e73b259
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-12 22:09:13 +0300

    add cards list and satrtel btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,12 @@ let startEl = document.getElementById("start")
 let newEl = document.getElementById("new")
 let nameEl = document.getElementById("name")
 document.title = ("BlackJack")
+const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+startEl.addEventListener("click",function(){
+    let card01 = Math.floor( Math.random(cards)*cards.length)
+    let card02 = Math.floor( Math.random(cards)*cards.length)
+    cardEl.innerHTML = `Card: ${cards[card01]} ${cards[card02]}`
+    
+    
+
+})
