4c31b2e9f9e88e95d9b5974fe82589e8020f501f
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-13 21:36:38 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let startEl = document.getElementById("start")
 let newEl = document.getElementById("new")
 let resetEl = document.getElementById("reset")
 let nameEl = document.getElementById("name")
+
 document.title = ("BlackJack")
 const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 nameEl.innerHTML = `HASSAN:$200`
@@ -30,9 +31,10 @@ startEl.addEventListener("click",function(){
             let sum = cards[card01] + cards[card02] + cards[i]
             sumEl.innerHTML = `Sum: ${sum}`
             
-           
             if(sum == 21){
                 playEl.innerHTML = `YOU ARE A WINNER`
+            }else if(sum < 21){
+                playEl.innerHTML = `pick onother card`
             }
             else{
                 playEl.innerHTML = `YOU ARE A LOOSER`
