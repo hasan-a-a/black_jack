c56d060d43cda15a7bd1fe9d7dfd6ceeb72d197e
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-13 21:37:40 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ let startEl = document.getElementById("start")
 let newEl = document.getElementById("new")
 let resetEl = document.getElementById("reset")
 let nameEl = document.getElementById("name")
-
 document.title = ("BlackJack")
 const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 nameEl.innerHTML = `HASSAN:$200`
